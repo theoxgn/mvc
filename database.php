a8e258648f27f0b5d6f9fd27ba9833cde8174495
theoxgn <aaluff112@gmail.com>
2022-08-28 14:51:45 +0700

    Update database.php

--- database.php
+++ database.php
@@ -6,35 +6,29 @@ class database{
 	var $uname = "root";
 	var $pass = "";
 	var $db = "test";
-
+  var $con;
 
 	public function __construct(){
-		$con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-		mysqli_select_db($con,$this->db);
+		$this->con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
+		mysqli_select_db($this->con,$this->db);
 	}
 
 	function tampil_data(){
-		$con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-		mysqli_select_db($con,$this->db);
-		$data = mysqli_query($con, "SELECT * FROM pegawai");
+		$data = mysqli_query($this->con, "SELECT a.id_peg,a.nama,a.alamat,a.no_hp,b.nama_jabatan FROM pegawai a INNER JOIN jabatan b ON a.id_jabatan=b.id_jabatan");
 		while($d = mysqli_fetch_array($data)){
 			$hasil[] = $d;
 		}
 		return $hasil;
 	}
 
-  function tambah_pegawai($id_peg, $nama, $alamat, $hp)
+  function tambah_pegawai($nama, $alamat, $hp)
   {
-    $con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-    mysqli_select_db($con, $this->db);
-    mysqli_query($con, "INSERT INTO PEGAWAI (nama,alamat,no_hp) VALUES('$id_peg','$nama','$alamat','$hp')");
+    mysqli_query($this->con, "INSERT INTO PEGAWAI (nama,alamat,no_hp) VALUES('$nama','$alamat','$hp')");
   }
 
   function get_pegawai_by_id($id_peg)
   {
-    $con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-    mysqli_select_db($con, $this->db);
-    $data = mysqli_query($con, "SELECT * FROM pegawai WHERE id_peg='$id_peg'");
+    $data = mysqli_query($this->con, "SELECT * FROM pegawai WHERE id_peg='$id_peg'");
     while ($d = mysqli_fetch_array($data)) {
       $hasil[] = $d;
     }
@@ -43,15 +37,20 @@ class database{
 
   function update_pegawai($id_peg, $nama, $alamat, $hp)
   {
-    $con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-    mysqli_select_db($con, $this->db);
-    mysqli_query($con, "UPDATE pegawai SET nama='$nama', alamat='$alamat', no_hp='$hp' WHERE id_peg='$id_peg'");
+    mysqli_query($this->con, "UPDATE pegawai SET nama='$nama', alamat='$alamat', no_hp='$hp' WHERE id_peg='$id_peg'");
   }
 
 	function hapus_pegawai($id){
-		$con = new mysqli($this->host, $this->uname, $this->pass, $this->db);
-		mysqli_select_db($con,$this->db);
-		mysqli_query($con, "DELETE FROM pegawai WHERE id_peg='$id'");
+		mysqli_query($this->con, "DELETE FROM pegawai WHERE id_peg='$id'");
 	}
+
+  function get_jabatan_by_id()
+  {
+    $data = mysqli_query($this->con, "SELECT * FROM jabatan");
+    while ($d = mysqli_fetch_array($data)) {
+      $hasil[] = $d;
+    }
+    return $hasil;
+  }
 }
 ?>
