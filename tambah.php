0f9ff24aa641da526184b4fba1a867c1757d5bc7
theoxgn <aaluff112@gmail.com>
2022-08-28 14:52:52 +0700

    Update tambah.php

--- tambah.php
+++ tambah.php
@@ -1,7 +1,12 @@
+<?php
+include 'database.php';
+$db = new database;
+?>
 <h1> welcome </h1>
 <h3> Tambah Data pegawai </h3>
 
 <form action="aksi.php?aksi=tambah" method="post">
+
     <table>
         <!-- <tr>
             <td> ID_Peg</td>
@@ -19,6 +24,19 @@
             <td> No hp</td>
             <td><input type="number" name="hp"></td>
         </tr>
+        <?php
+        $no = 1;
+        foreach ($db->get_jabatan_by_id() as $key) { //error disini
+        ?>
+            <tr>
+                <td> Jabatan
+                <td>
+                    <select id="id_jabatan" name="id_jabatan">
+                        <option value="<?php echo $key['id_jabatan']; ?>"><?php echo $key['nama_jabatan']; ?></option>
+                    </select>
+                </td>
+            </tr>
+        <?php } ?>
         <tr>
             <td></td>
             <td><input type="submit" value="simpan"></td>
