f34e509a0ff61c00a8c463efc48ea97e340fa59f
theoxgn <aaluff112@gmail.com>
2022-08-28 14:54:03 +0700

    Update test.sql

--- test.sql
+++ test.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 26, 2022 at 03:44 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: localhost
+-- Waktu pembuatan: 28 Agu 2022 pada 09.53
+-- Versi server: 10.4.21-MariaDB
+-- Versi PHP: 7.4.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,43 +24,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pegawai`
+-- Struktur dari tabel `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nama_jabatan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `jabatan`
+--
+
+INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`) VALUES
+(1, 'Pegawai');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pegawai`
 --
 
 CREATE TABLE `pegawai` (
   `id_peg` int(10) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `alamat` varchar(500) NOT NULL,
-  `no_hp` varchar(15) NOT NULL
+  `no_hp` varchar(15) NOT NULL,
+  `id_jabatan` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pegawai`
+-- Dumping data untuk tabel `pegawai`
 --
 
-INSERT INTO `pegawai` (`id_peg`, `nama`, `alamat`, `no_hp`) VALUES
-(6, 'Mesut Ozil 2 edwd', 'Jalan Sebukuwww', '123211122111'),
-(7, 'Budi Santoso55', 'Jalan Sebuku', '1232111');
+INSERT INTO `pegawai` (`id_peg`, `nama`, `alamat`, `no_hp`, `id_jabatan`) VALUES
+(7, 'Budi Santo', 'Jalan Sebuku', '1232111', 1),
+(8, 'juno', 'mjw', '081', 1),
+(9, 'Rinda', 'jombang', '0830152503', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `pegawai`
+-- Indeks untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`id_jabatan`);
+
+--
+-- Indeks untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
   ADD PRIMARY KEY (`id_peg`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `pegawai`
+-- AUTO_INCREMENT untuk tabel `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `pegawai`
 --
 ALTER TABLE `pegawai`
-  MODIFY `id_peg` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_peg` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
