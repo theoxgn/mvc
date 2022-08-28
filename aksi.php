4153ebe4b4ecc68339f3cccfee6a87fc18712a57
theoxgn <aaluff112@gmail.com>
2022-08-28 14:51:26 +0700

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -4,10 +4,10 @@ $db = new database;
 $aksi = $_GET['aksi'];
 
 if ($aksi == "tambah") {
-  $db->tambah_pegawai($_POST['id_peg'], $_POST['nama'], $_POST['alamat'], $_POST['hp']);
+  $db->tambah_pegawai($_POST['nama'], $_POST['alamat'], $_POST['hp'], $_POST['id_jabatan']);
   header("location:index.php");
 } elseif ($aksi == "edit") {
-  $db->update_pegawai($_POST['id_peg'], $_POST['nama'], $_POST['alamat'], $_POST['hp']);
+  $db->update_pegawai($_POST['id_peg'], $_POST['nama'], $_POST['alamat'], $_POST['hp'],$_POST['id_jabatan']);
   header("location:index.php");
 } elseif ($aksi == "hapus") {
   // if ($_GET['id_peg'] != 0) {
